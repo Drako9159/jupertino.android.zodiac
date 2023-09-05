04112a744eafd74d3840d2286b872e9ff1cf6141
drako9159 <chochito9159@gmail.com>
2023-09-05 15:13:12 -0600

    feat(colors)define colors

--- app/src/main/java/com/drako9159/horoscoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/drako9159/horoscoapp/ui/home/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.drako9159.horoscoapp
+package com.drako9159.horoscoapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.drako9159.horoscoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
