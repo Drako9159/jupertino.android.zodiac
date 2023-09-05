01c2faec28ba1226a2d47773f0b9c5d2fe9bb975
drako9159 <chochito9159@gmail.com>
2023-09-05 17:23:06 -0600

    feat(navigation) configure navigation

--- app/src/main/java/com/drako9159/horoscoapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/drako9159/horoscoapp/ui/luck/LuckFragment.kt
@@ -15,7 +15,7 @@ class LuckFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
         return binding.root
--- app/src/main/java/com/drako9159/horoscoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/drako9159/horoscoapp/ui/palmistry/PalmistryFragment.kt
@@ -1,12 +1,10 @@
 package com.drako9159.horoscoapp.ui.palmistry
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.drako9159.horoscoapp.R
-import com.drako9159.horoscoapp.databinding.FragmentLuckBinding
+import androidx.fragment.app.Fragment
 import com.drako9159.horoscoapp.databinding.FragmentPalmistryBinding
 
 
@@ -18,7 +16,7 @@ class PalmistryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
