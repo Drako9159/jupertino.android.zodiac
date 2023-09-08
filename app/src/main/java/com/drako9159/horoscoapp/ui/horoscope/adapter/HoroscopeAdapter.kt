fb0b803e0879d87253be01114e84d51599dc6800
drako9159 <chochito9159@gmail.com>
2023-09-07 19:23:54 -0600

    feat(view) configure recycer view

---
+++ app/src/main/java/com/drako9159/horoscoapp/ui/horoscope/adapter/HoroscopeAdapter.kt
@@ -0,0 +1,20 @@
+package com.drako9159.horoscoapp.ui.horoscope.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.drako9159.horoscoapp.R
+import com.drako9159.horoscoapp.domain.model.HoroscopeInfo
+
+class HoroscopeAdapter(private var horoscopeList: List<HoroscopeInfo> = emptyList()) :
+    RecyclerView.Adapter<HoroscopeViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HoroscopeViewHolder {
+        return HoroscopeViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_horoscope, parent, false))
+    }
+
+    override fun getItemCount() = horoscopeList.size
+
+    override fun onBindViewHolder(holder: HoroscopeViewHolder, position: Int) {
+        holder.render(horoscopeList[position])
+    }
+}
---
+++ app/src/main/java/com/drako9159/horoscoapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -0,0 +1,11 @@
+package com.drako9159.horoscoapp.ui.horoscope.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.drako9159.horoscoapp.domain.model.HoroscopeInfo
+
+class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun render(horoscopeInfo: HoroscopeInfo) {
+
+    }
+}
