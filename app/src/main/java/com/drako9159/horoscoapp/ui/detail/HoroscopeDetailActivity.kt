474f68150ac68b3c1e2a4b03e0f810875fa40d4d
drako9159 <chochito9159@gmail.com>
2023-09-09 18:32:28 -0600

    feat(adjust)

--- app/src/main/java/com/drako9159/horoscoapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/drako9159/horoscoapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,10 +3,15 @@ package com.drako9159.horoscoapp.ui.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.drako9159.horoscoapp.databinding.ActivityHoroscopeDetailBinding
-import com.drako9159.horoscoapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -14,12 +19,33 @@ class HoroscopeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHoroscopeDetailBinding
     private val horoscopeDetailViewModel: HoroscopeDetailViewModel by viewModels()
 
-    private val args:HoroscopeDetailActivityArgs by navArgs()
+    private val args: HoroscopeDetailActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         args.type
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopeDetailViewModel.state.collect{
+                    when(it){
+                        is HoroscopeDetailState.Error -> TODO()
+                        HoroscopeDetailState.Loading -> {
+                            binding.pb.isVisible = true
+                        }
+                        is HoroscopeDetailState.Success -> TODO()
+                    }
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/drako9159/horoscoapp/ui/detail/HoroscopeDetailState.kt
@@ -0,0 +1,7 @@
+package com.drako9159.horoscoapp.ui.detail
+
+sealed class HoroscopeDetailState {
+    data object Loading : HoroscopeDetailState()
+    data class Error(val error: String) : HoroscopeDetailState()
+    data class Success(val data: String) : HoroscopeDetailState()
+}
