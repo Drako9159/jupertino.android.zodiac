8aa14dcc2c1d47c74a20b4836b968f036f9f1826
drako9159 <chochito9159@gmail.com>
2023-09-15 13:25:24 -0600

    test(mother) mother object

--- app/src/test/java/com/drako9159/horoscoapp/data/network/response/PredictionResponseTest.kt
+++ app/src/test/java/com/drako9159/horoscoapp/data/network/response/PredictionResponseTest.kt
@@ -1,16 +1,18 @@
 package com.drako9159.horoscoapp.data.network.response
 
 import com.drako9159.horoscoapp.domain.model.HoroscopeModel
+import com.drako9159.horoscoapp.motherobject.HoroscopeMotherObject
 import io.kotlintest.shouldBe
 import org.junit.Assert.*
 import org.junit.Test
+import kotlin.math.sign
 
 class PredictionResponseTest{
 
     @Test
     fun `toDomain should return a correct PredictionModel`(){
         //Given
-        val horoscopeResponse = PredictionResponse("date", "prediction", "taurus")
+        val horoscopeResponse = HoroscopeMotherObject.anyResponse.copy(sign = "taurus")
 
         //When
         val predictionModel = horoscopeResponse.toDomain()
---
+++ app/src/test/java/com/drako9159/horoscoapp/motherobject/HoroscopeMotherObject.kt
@@ -0,0 +1,8 @@
+package com.drako9159.horoscoapp.motherobject
+
+import com.drako9159.horoscoapp.data.network.response.PredictionResponse
+
+object HoroscopeMotherObject {
+
+    val anyResponse = PredictionResponse("date","prediction", "leo")
+}
